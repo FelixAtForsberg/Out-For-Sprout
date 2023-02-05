3d3de1b3c0895c90c8cc500a1482ab7bd421212f
Sofie Bresäter <sbresater1337@gmail.com>
2023-02-05 12:05:04 +0100

    ui

--- src/Out For Sprout/Assets/UIManager.cs
+++ src/Out For Sprout/Assets/UIManager.cs
@@ -15,11 +15,11 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject pauseButton;
     [SerializeField] private Sprite pauseSprite;
     [SerializeField] private Sprite playSprite;
-    [SerializeField] private TextMeshPro score;
-    [SerializeField] private TextMeshPro bestScore;
+    [SerializeField] private GameObject score;
+    [SerializeField] private GameObject bestScore;
 
 
-    private bool isGameOver = false;
+    //private bool isGameOver = false;
     private bool isPaused = false;
     
     void Awake(){
@@ -32,16 +32,57 @@ public class UIManager : MonoBehaviour
         joyStick.SetActive(false);
         winPanel.SetActive(false);
 
-        GameManager.Instance.OnPlayerDeath.AddListener(SetGameOver);             
+        GameManager.Instance.OnPlayerDeath.AddListener(SetGameOver);    
+        GameManager.Instance.OnPlayerWin.AddListener(SetWin);
+
+       
+    
     }
 
+    void Update(){
 
+        if(Input.GetKeyDown(KeyCode.A)){
+
+            SetWin();
+        }
+    }
 
     void SetGameOver(){
         
-        //Debug.Log(ProgressTracker.Instance.GetTimer());
         StartCoroutine(GameOverSequence());
         
+    }
+
+    void SetWin(){
+        Time.timeScale = 0;
+        winPanel.SetActive(true);
+
+
+        float timer = ProgressTracker.Instance.GetTimer();
+        score.GetComponent<TMP_Text>().text = Timeformat(timer);
+
+        
+        float bestFloat = PlayerPrefs.GetFloat("myFloat", float.MaxValue);
+        if(bestFloat >= float.MaxValue){
+            bestScore.GetComponent<TMP_Text>().text = "";
+        }
+        if(bestFloat>timer){
+            
+            PlayerPrefs.SetFloat("myFloat", timer);
+            bestScore.GetComponent<TMP_Text>().text = Timeformat(timer);
+        }
+        if(bestFloat<timer){
+            bestScore.GetComponent<TMP_Text>().text = Timeformat(bestFloat);
+
+        }        
+
+    }
+
+    private string Timeformat(float timer){
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        string time = string.Format("{0:00}:{1:00}", minutes, seconds);
+        return time;
     }
 
     public void StartGame(){
@@ -76,7 +117,6 @@ public class UIManager : MonoBehaviour
 
     private IEnumerator GameOverSequence(){
         gameOverPanel.SetActive(true);
-
         
         Time.timeScale = 0;
         yield return new WaitForSeconds(5.0f);
