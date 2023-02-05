5f0f802731ad87904342c9473c0cbb19700d1aef
Jaws <christian_markowicz@hotmail.com>
2023-02-05 11:52:17 +0100

    Slowdown by bugs

--- src/Out For Sprout/Assets/5-Scripts/Player/PlayerMovement.cs
+++ src/Out For Sprout/Assets/5-Scripts/Player/PlayerMovement.cs
@@ -23,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private AnimationCurve speedCurveByPercentageOfMap;
 
     private List<SpeedBuff> activeBuffs = new List<SpeedBuff>();
+    private List<SpeedBuff> activeDebuffs = new List<SpeedBuff>();
 
     private void Awake()
     {
@@ -69,12 +70,18 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateBuffs()
     {
-        for (int i = 0; i < activeBuffs.Count; i++)
+        UpdateBuffTimers(activeBuffs);
+        UpdateBuffTimers(activeDebuffs);
+    }
+
+    private void UpdateBuffTimers(List<SpeedBuff> buffs)
+    {
+        for (int i = 0; i < buffs.Count; i++)
         {
-            activeBuffs[i].timer += Time.deltaTime;
-            if (activeBuffs[i].timer >= activeBuffs[i].totalTime)
+            buffs[i].timer += Time.deltaTime;
+            if (buffs[i].timer >= buffs[i].totalTime)
             {
-                activeBuffs.RemoveAt(i);
+                buffs.RemoveAt(i);
                 i--;
             }
         }
@@ -83,9 +90,10 @@ public class PlayerMovement : MonoBehaviour
     private void UpdateMovingDirection()
     {
         var inputDirection = gameActions.Player.Move.ReadValue<Vector2>();
-        var getSpeedBonus = GetSpeedBonusByBuffs();
+        var getSpeedBonus = GetSpeedBonusByBuffs(activeBuffs);
+        var getSpeedSLowdown = GetSpeedBonusByBuffs(activeDebuffs);
         
-        var newVelocity = GetSpeedForProgress() + getSpeedBonus;
+        var newVelocity = Mathf.Max(0, GetSpeedForProgress() + getSpeedBonus + getSpeedSLowdown);
 
         var finalDirection = lastDirection;
         if (inputDirection.magnitude > 0)
@@ -97,10 +105,10 @@ public class PlayerMovement : MonoBehaviour
         lastDirection = rigidbody.velocity.normalized;
     }
 
-    private float GetSpeedBonusByBuffs()
+    private float GetSpeedBonusByBuffs(List<SpeedBuff> buffs)
     {
         var totalSpeedBonus = 0f;
-        foreach (var buff in activeBuffs)
+        foreach (var buff in buffs)
         {
             var alpha = 1.0f - (buff.timer / buff.totalTime);
             totalSpeedBonus += buff.startSpeedBonus * alpha;
@@ -112,5 +120,10 @@ public class PlayerMovement : MonoBehaviour
     public void AddSpeedupBuff(float speedBonus, float fadeoutTime)
     {
         activeBuffs.Add(new SpeedBuff(fadeoutTime, speedBonus));
+    }
+
+    public void AddSpeedupDebuff(float speedPenalty, float fadeoutTime)
+    {
+        activeDebuffs.Add(new SpeedBuff(fadeoutTime, speedPenalty));
     }
 }
